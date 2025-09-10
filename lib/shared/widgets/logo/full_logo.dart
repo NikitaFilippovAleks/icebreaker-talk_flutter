f1cfbc981d6ffb0122f174022c1952355fbc3546
Nikita Filippov <nikita.filippov.aleks@mail.ru>
2025-09-11 00:13:23 +0300

    Logo animation

--- lib/shared/widgets/logo/full_logo.dart
+++ lib/shared/widgets/logo/full_logo.dart
@@ -4,8 +4,52 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../app/gen/assets.gen.dart';
 import '../loader/index.dart';
 
-class FullLogo extends StatelessWidget {
+class FullLogo extends StatefulWidget {
   const FullLogo({super.key});
+
+  @override
+  State<FullLogo> createState() => _FullLogoState();
+}
+
+class _FullLogoState extends State<FullLogo> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimationIcebreaker;
+  late Animation<double> _opacityAnimationTalk;
+  late Animation<double> _opacityAnimationLogo;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(duration: const Duration(milliseconds: 2000), vsync: this);
+    _opacityAnimationIcebreaker = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.6, 1.0, curve: Curves.easeIn),
+      ),
+    );
+    _opacityAnimationTalk = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.3, 0.5, curve: Curves.easeIn),
+      ),
+    );
+    _opacityAnimationLogo = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0, 0.2, curve: Curves.easeIn),
+      ),
+    );
+
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => IntrinsicWidth(
@@ -17,14 +61,35 @@ class FullLogo extends StatelessWidget {
             top: 0,
             right: 38.w,
             bottom: 0,
-            child: const Center(child: Loader(size: 38, isAnimated: false)),
+            child: Center(
+              child: AnimatedBuilder(
+                animation: _opacityAnimationLogo,
+                builder: (context, child) => Opacity(
+                  opacity: _opacityAnimationLogo.value,
+                  child: const Loader(size: 38, isAnimated: false),
+                ),
+              ),
+            ),
           ),
+
           Center(
             child: Row(
               spacing: 16.w,
               children: [
-                Assets.icons.logoLetters.icebreaker.svg(height: 13.h),
-                Assets.icons.logoLetters.talk.svg(height: 13.h, width: 28.w),
+                AnimatedBuilder(
+                  animation: _opacityAnimationIcebreaker,
+                  builder: (context, child) => Opacity(
+                    opacity: _opacityAnimationIcebreaker.value,
+                    child: Assets.icons.logoLetters.icebreaker.svg(height: 13.h),
+                  ),
+                ),
+                AnimatedBuilder(
+                  animation: _opacityAnimationTalk,
+                  builder: (context, child) => Opacity(
+                    opacity: _opacityAnimationTalk.value,
+                    child: Assets.icons.logoLetters.talk.svg(height: 13.h, width: 28.w),
+                  ),
+                ),
               ],
             ),
           ),
